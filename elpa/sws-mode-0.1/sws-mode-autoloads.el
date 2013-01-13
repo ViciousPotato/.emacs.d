968944e425d39ba7a3fc7ebaed8a3d3a47385661
Zhou Honglin <zhou.honglin@gmail.com>
2013-01-13 22:59:39 +0800

    Add plugins

---
+++ elpa/sws-mode-0.1/sws-mode-autoloads.el
@@ -0,0 +1,18 @@
+;;; sws-mode-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+
+;;;### (autoloads nil nil ("sws-mode-pkg.el" "sws-mode.el") (20720
+;;;;;;  62011 723000 0))
+
+;;;***
+
+(provide 'sws-mode-autoloads)
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; sws-mode-autoloads.el ends here
--- init.el
+++ init.el
@@ -1,15 +1,24 @@
 ;; make it cleaner
+(when (functionp 'tool-bar-mode)
+	(tool-bar-mode -1))
 (tool-bar-mode 0)
 (setq inhibit-statrtup-message t)
 (setq initial-scratch-message nil)
 
 ;; space
+(setq-default tab-width 2)
+(setq-default indent-tabs-mode nil)
 
 ;; no backup
 (setq backup-enable-predicate
       (lambda (name) nil))
 
+;; encoding
+(prefer-coding-system 'utf-8)
+
 ;; theme
+(set-default-font "Monaco-10")
+(setq-default line-spacing 3)
 (add-to-list 'custom-theme-load-path "~/.emacs.d/themes")
 (load-theme 'bubbleberry t)
 
@@ -19,8 +28,39 @@
 (require 'cl)
 (require 'powerline)
 
+;; proxy
+(setq url-using-proxy t)
+(setq url-proxy-services  '(("http" . "proxy.fudan.edu.cn:8080")))
+
 ;; Emacs Starter Kit
 (require 'package)
 (add-to-list 'package-archives
              '("marmalade" . "http://marmalade-repo.org/packages/") t)
 (package-initialize)
+
+
+;; ecb
+(setq stack-trace-on-error nil)
+(setq ecb-tip-of-the-day nil)
+(require 'ecb)
+(ecb-activate)
+
+;; Language modes
+;; Jade mode
+(require 'sws-mode)
+(require 'jade-mode)    
+(add-to-list 'auto-mode-alist '("\\.styl$" . sws-mode))
+(add-to-list 'auto-mode-alist '("\\.jade$" . jade-mode))
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(ecb-options-version "2.40"))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
